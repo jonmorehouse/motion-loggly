138b2432587de23e324c3426b4cb29b8ddb35138
Jon Morehouse <morehousej09@gmail.com>
2014-06-18 20:15:43 -0700

    Temp commit

--- lib/loggly/bulk.rb
+++ lib/loggly/bulk.rb
@@ -1,14 +1,19 @@
-module LogglyAPI
+module LogglyAPI 
 
-  class Bulk
+  class Bulk < Base
+
+    @@endpoint = "bulk"
 
     # expects an array of objects
     # assumes that you are running a bit more sophisticated. eg: doesn't include timestamps automatically
     # expects data to be an array of hashes
-    def send(data, opts = {})
-
-      #raise ArgumentError, "invalid method name" unless METHODS.include? @request_method
-
+    def send(msgs, opts = {}, &cb)
+      raise ArgumentError, "invalid method name" unless msgs.kind_of?(Array)
+      @cb = cb
+      tags = normalize_tags opts
+      url = build_url(tags)
+      puts url
+      @cb.call("")
     end
 
   end
--- spec/bulk_spec.rb
+++ spec/bulk_spec.rb
@@ -2,23 +2,37 @@ describe "Event" do
   extend WebStub::SpecHelpers
   
   before do
-    @event = LogglyAPI::Event.new TOKEN, :tags => "default"
+    @bulk = LogglyAPI::Bulk.new TOKEN, :tags => "default"
+    @stub = stub_request(:post, "http://logs-01.loggly.com/bulk/TOKEN/tag/default")
+    @stub.to_return(json: {:response => "ok"})
+    @msgs = []
+      #{
+        #:msg => "msg",
+        #:timestamp => "timestamp"
+      #},
+      #{
+        #:msg => "msg",
+        #:timestamp => "timestamp"
+      #}
+    #]
+
   end
 
-  it "should build a post request and call it with the request class" do
-    stub = stub_request(:post, "http://logs-01.loggly.com/inputs/TOKEN/tag/default")
-    stub.to_return(json: {:response => "ok"})
+  it "should request the loggly api with an array of hashes" do
+    
+    #@runner.call()
 
-    @event.send("msg") do |result|
-      @result = result
-      resume
-    end
     wait_max 1.0 do
-      @result.success?.should == true
-      # super hack -> ...
-      @result.body.should == "{\"response\":\"ok\"}"
+      puts "finished"
+      1.should == 1
     end
+
   end
+
+  #it "should raise an error when not passed an array" do
+
+  #end
+
 
 end
 
