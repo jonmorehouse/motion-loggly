f7f2941c3bcb138189c816ebf68529df352f28e4
Jon Morehouse <morehousej09@gmail.com>
2014-06-20 13:22:21 -0700

    Add in some code to help debug afmotion

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -21,18 +21,57 @@ class AppDelegate
 
   end
 
-  def self.test_call
+  @token = "a1a8d452-281f-40aa-ac4e-ff29d1e0a0e0"
 
-    # curl --data  '{"msg":"value"}' -H 'Content-Type: application/json' http://logs-01.loggly.com/inputs/TOKEN/tag/msg,default
-    url = "http://logs-01.loggly.com/inputs/TOKEN/tag/msg,default"
-    AFMotion::JSON.post(url, {:msg => "value"}) do |result|
-      puts "JSON was #{result.success?}"
+  @apiroot = "http://logs-01.loggly.com/"
+  @endpoint = "inputs/#{@token}/tag/msg,default"
+
+
+  @url = "http://logs-01.loggly.com/inputs/#{@token}/tag/msg,default"
+  @data = {:msg => "message"}
+
+  def self.bubblewrap
+
+    BW::HTTP.post("#{@url}", {payload: @data, format: :json}) do |result|
+      puts "Bubblewrap: #{result.ok?}"
+      puts "Bubblewrap Body: #{result.body}"
     end
-
-    AFMotion::HTTP.post(url, {:msg => "value"}) do |result|
-      puts "HTTP was #{result.success?}"
-    end
-
 
   end
+
+  def self.afmotion
+
+    # curl --data  '{"msg":"value"}' -H 'Content-Type: application/json' http://logs-01.loggly.com/inputs/TOKEN/tag/msg,default
+    AFMotion::JSON.post(@url, @data) do |result|
+      puts "JSON was #{result.success?}"
+      puts "Body #{result.object}"
+      puts "Body #{result.error.localizedDescription}"
+    end
+
+    #AFMotion::HTTP.post(url, {:msg => "value"}) do |result|
+      #puts "HTTP was #{result.success?}"
+    #end
+  end
+
+  def self.client
+
+    client = AFMotion::Client.build(@apiroot) do
+
+      header "Content-Type", "application/json"
+      request_serializer :json
+      header "Accept", "text/html"
+      #response_serializer :json
+      
+    end
+
+    client.post(@endpoint, @data) do |result|
+
+      puts result.success?
+      puts result.error.localizedDescription 
+      @result = result
+
+    end
+  end
+
+
 end
